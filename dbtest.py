173afdae884696b3d3205ba10c9d53f8860c3812
Joe Schumacher <joe.schumacher@nokia.com>
2019-06-07 23:40:53 -0500

    non-specific updates

--- dbtest.py
+++ dbtest.py
@@ -1,17 +1,23 @@
+import sys
 import sqlite3
-conn = sqlite3.connect('example.db')
-c = conn.cursor()
+
+#conn = sqlite3.connect('example.db')
+#c = conn.cursor()
 
 # Create table
-c.execute('''CREATE TABLE stocks
-             (date text, trans text, symbol text, qty real, price real)''')
+#c.execute('''CREATE TABLE stocks
+#             (date text, trans text, symbol text, qty real, price real)''')
 
 # Insert a row of data
-c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+#c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
 
 # Save (commit) the changes
-conn.commit()
+#conn.commit()
 
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
-conn.close()
+#conn.close()
+
+p = sys.argv
+print('{} arguments'.format(len(p)))
+print( p )
